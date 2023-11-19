9c25a213493fd01cbac615e015448890516d02ae
dac2 <dac2@cesar.org.br>
2023-11-19 11:21:09 -0300

    Initial files

---
+++ src/main/java/dev/danielarrais/votingsystem/VotingSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.danielarrais.votingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingSystemApplication.class, args);
+    }
+
+}
