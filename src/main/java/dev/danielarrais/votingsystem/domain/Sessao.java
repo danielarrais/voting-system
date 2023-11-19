863631ec4f9ecc7b4d22757882e19356ae0a1eca
dac2 <dac2@cesar.org.br>
2023-11-19 15:40:17 -0300

    Finaliza modelos

--- src/main/java/dev/danielarrais/votingsystem/domain/Sessao.java
+++ src/main/java/dev/danielarrais/votingsystem/domain/Sessao.java
@@ -1,7 +1,9 @@
 package dev.danielarrais.votingsystem.domain;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,7 +12,11 @@ import java.util.List;
 @Getter
 public class Sessao {
     private Long id;
+
+    @Setter(AccessLevel.NONE)
     private LocalDateTime dataInicio;
+
+    @Setter(AccessLevel.NONE)
     private LocalDateTime dataEncerramento;
     private List<Voto> votos;
 
--- src/main/java/dev/danielarrais/votingsystem/domain/Voto.java
+++ src/main/java/dev/danielarrais/votingsystem/domain/Voto.java
@@ -2,13 +2,11 @@ package dev.danielarrais.votingsystem.domain;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Builder
 @Getter
-@Setter
 public class Voto {
     private Long id;
     private String CPF;
