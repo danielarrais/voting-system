c1f35dce78e79779cbef7cbafa2721a51f51472c
Daniel Arrais <danielarraiscarvalho@gmail.com>
2023-11-20 14:33:49 -0300

    Ajusta client de verificação de CPF

--- src/main/java/dev/danielarrais/votingsystem/infra/feign/InvertextClient.java
+++ src/main/java/dev/danielarrais/votingsystem/infra/feign/InvertextClient.java
@@ -4,10 +4,11 @@ package dev.danielarrais.votingsystem.infra.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "invertext-com", url = "${invert-text.base-url}", configuration = InvertextConfiguration.class)
 public interface InvertextClient {
 
-    @GetMapping(name = "/validator")
-    CPFValido cpfValido(@PathVariable(name = "value") String CPF);
+    @GetMapping(path = "/validator")
+    CPFValido cpfValido(@RequestParam(name = "value") String CPF);
 }
