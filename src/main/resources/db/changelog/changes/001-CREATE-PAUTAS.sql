7fbf84f2ce9f91e50b5986a65f5a655335fe7690
Daniel Arrais <danielarraiscarvalho@gmail.com>
2023-11-20 07:25:54 -0300

    Remove assembleia

---
+++ src/main/resources/db/changelog/changes/001-CREATE-PAUTAS.sql
@@ -0,0 +1,6 @@
+create table pautas
+(
+    id            int auto_increment primary key,
+    descricao     varchar(255) not null,
+    titulo        varchar(255) not null
+);
--- src/main/resources/db/changelog/changes/004-CREATE-VOTOS.sql
+++
@@ -1,9 +0,0 @@
-create table votos
-(
-    id        int primary key,
-    voto      bool         not null,
-    hora      datetime(6)  not null,
-    pauta_id int          null,
-    cpf       varchar(255) not null,
-    foreign key (pauta_id) references votos (id)
-);
