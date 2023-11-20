17c519e81452df9bee53cdda74c204f483db8f28
Daniel Arrais <danielarraiscarvalho@gmail.com>
2023-11-20 14:32:04 -0300

    Ajusta use case de criação de pautas para evitar duplicidade

--- src/main/java/dev/danielarrais/votingsystem/application/CriarSessaoService.java
+++ src/main/java/dev/danielarrais/votingsystem/application/CriarSessaoService.java
@@ -1,6 +1,9 @@
 package dev.danielarrais.votingsystem.application;
 
+import dev.danielarrais.votingsystem.application.exceptions.PautaComSessaoJaRegistrada;
 import dev.danielarrais.votingsystem.application.exceptions.PautaNaoEncontrada;
+import dev.danielarrais.votingsystem.application.exceptions.PautaSemSessaoAberta;
+import dev.danielarrais.votingsystem.infra.database.entities.PautaEntity;
 import dev.danielarrais.votingsystem.infra.database.entities.SessaoEntity;
 import dev.danielarrais.votingsystem.infra.database.repositories.PautaRepository;
 import dev.danielarrais.votingsystem.infra.database.repositories.SessaoRepository;
@@ -15,9 +18,11 @@ public class CriarSessaoService {
     private final PautaRepository pautaRepository;
     private final SessaoRepository sessaoRepository;
 
-    public void cria(Long pautaID, Integer duracao) {
-        validarSePautaExiste(pautaID);
+    public void cria(Long pautaId, Integer duracao) {
+        validaSePautaExiste(pautaId);
+        validaUnicidadeDaSessao(pautaId);
 
+        PautaEntity pautaEntity = buscarPauta(pautaId);
         LocalDateTime dataInicio = LocalDateTime.now();
 
         // TODO: CRIAR DURACAO DEFAULT DE 1 MINUTO
@@ -26,16 +31,31 @@ public class CriarSessaoService {
         SessaoEntity sessaoEntity = SessaoEntity.builder()
                 .dataInicio(dataInicio)
                 .dataEncerramento(dataEncerramento)
+                .pauta(pautaEntity)
                 .build();
 
         sessaoRepository.save(sessaoEntity);
     }
 
-    private void validarSePautaExiste(Long pautaId) {
+    private void validaSePautaExiste(Long pautaId) {
         boolean pautaExiste = pautaRepository.existsById(pautaId);
+
         if (!pautaExiste) {
             throw new PautaNaoEncontrada(pautaId);
         }
+    }
+
+    private void validaUnicidadeDaSessao(Long pautaId) {
+        boolean jaExisteSessao = sessaoRepository.existsByPautaId(pautaId);
+
+        if (jaExisteSessao) {
+            throw new PautaComSessaoJaRegistrada(pautaId);
+        }
+
+    }
+
+    private PautaEntity buscarPauta(Long pautaId) {
+        return pautaRepository.findById(pautaId).orElseThrow(() -> new PautaNaoEncontrada(pautaId));
     }
 
     private LocalDateTime gerarDataEncerramento(Integer duracao, LocalDateTime dataInicio) {
