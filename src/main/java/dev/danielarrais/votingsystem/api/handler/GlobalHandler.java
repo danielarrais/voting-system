9c13b4a5cd74dbc65540f94e5fe64261c784b3e5
Daniel Arrais <danielarraiscarvalho@gmail.com>
2023-11-21 09:57:06 -0300

    Trata qualquer exceção

--- src/main/java/dev/danielarrais/votingsystem/api/handler/GlobalHandler.java
+++ src/main/java/dev/danielarrais/votingsystem/api/handler/GlobalHandler.java
@@ -2,6 +2,7 @@ package dev.danielarrais.votingsystem.api.handler;
 
 import dev.danielarrais.votingsystem.api.dto.response.ErroResponse;
 import dev.danielarrais.votingsystem.core.application.exceptions.NegocioException;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
@@ -15,14 +16,28 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+@Log4j2
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class GlobalHandler {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public ErroResponse generalException(Exception ex) {
+        log.error(ex.getMessage(), ex);
+        return ErroResponse.builder()
+                .codigo(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .mensagem(ex.getMessage())
+                .build();
+    }
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     @ExceptionHandler(NegocioException.class)
     public ErroResponse negocioExceptionHandler(NegocioException ex) {
+        log.error(ex.getMessage(), ex);
         return ErroResponse.builder()
                 .codigo(HttpStatus.BAD_REQUEST.value())
                 .mensagem(ex.getMessage())
@@ -33,6 +48,8 @@ public class GlobalHandler {
     @ResponseBody
     @ExceptionHandler(BindException.class)
     public ErroResponse bindValidationException(BindException ex) {
+        log.error(ex.getMessage(), ex);
+
         BindingResult result = ex.getBindingResult();
         List<String> erros = this.extrairErrosDeValidacao(result);
 
