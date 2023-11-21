2c2311c73056481c36a804ad551a3ee23deef309
Daniel Arrais <danielarraiscarvalho@gmail.com>
2023-11-21 12:20:15 -0300

    Ativa retry para tentativas em caso de indisponibilidades

--- src/main/java/dev/danielarrais/votingsystem/infra/feign/InvertextClient.java
+++ src/main/java/dev/danielarrais/votingsystem/infra/feign/InvertextClient.java
@@ -2,6 +2,8 @@ package dev.danielarrais.votingsystem.infra.feign;
 
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,5 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface InvertextClient {
 
     @GetMapping(path = "/validator")
+    @Retryable(maxAttempts = 5, backoff = @Backoff(delay = 1000))
     CpfValido cpfValido(@RequestParam(name = "value") String cpf);
 }
